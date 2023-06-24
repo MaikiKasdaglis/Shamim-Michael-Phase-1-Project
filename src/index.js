76cb07536f515615efd15a137f8aefbd91f5cd39
Michael Kasdaglis <maikikasdaglis@gmail.com>
2023-06-24 14:10:59 -0600

    working on getting some stuff on the page. formating and what not is still really bad

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch(`http://localhost:3000`)
-    .then(res => res.json)
+    fetch(` http://localhost:3000/current-stored-dreams`)
+    .then(res => res.json())
     .then(data => {
-        console.log(`dbjson werks`)
+        console.log(`this is the array we will push our dream objects too`, data)
+        data.forEach(dream => {
+            pastDreamBar(dream)
+            dreamImageBar(dream)
+        })
     })
 })
+let pastDreams =document.querySelector(`.stored-dreams`)
+function pastDreamBar(dream) {
+    let dreamTitle = document.createElement('h3')
+    let dreamTheme = document.createElement('h5')
+    let dreamRating = document.createElement('h5')
 
+    dreamTitle.innerText = dream.title
+    dreamTheme.innerText = dream.theme
+    dreamRating.innerText = dreamRating
+
+    pastDreams.append(dreamTitle, dreamTheme, dreamRating)
+}
+let photoBox = document.querySelector('.photo-box')
+function dreamImageBar(dream) {
+    let dreamImage = document.createElement('img')
+    dreamImage.src = dream.image
+    photoBox.appendChild(dreamImage)
+
+}
 
 
 
 
 button = document.querySelector('#text-button')
 dreamDetails = document.querySelector("#dream-text-area")
+titleEntry = document.querySelector('#title-entry')
 let dreamObject = {
     title: '',
     rating: '',
@@ -19,9 +42,16 @@ let dreamObject = {
     details: '',
     image: '',
 }
-button.addEventListener('submit', e => {
+button.addEventListener('click', e => {
     e.preventDefault()
     console.log(dreamDetails.value)
+    console.log(titleEntry.value)
     dreamObject.details = dreamDetails.value
+    dreamObject.details = titleEntry.value
     console.log(dreamObject)
 })
+
+
+
+
+
